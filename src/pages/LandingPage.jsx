a904f161d8c11149148607e5232f7e194c2c79f9
sahilkhan8786 <official.khansahil8786@gmail.com>
2024-09-25 15:49:06 +0530

    update landingpage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,12 +3,10 @@ import { Link } from 'react-router-dom'
 
 export default function LandingPage() {
     return (
-        <div>
-            <Link to={'/first'}>
-                <button>Go to first page</button>
-            </Link>
-            <Link to={'/second'}>
-                <button>Go to second page</button>
+        <div className='flex justify-center items-center w-full h-screen'>
+
+            <Link to={'/second'} className='bg-[#009ddb] text-white p-3 hover:bg-[#009ddbc5]'>
+                <button>Go to page</button>
             </Link>
         </div>
     )
